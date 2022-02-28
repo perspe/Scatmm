6018c235715fb7064ae9d54b8a1dffce4abcf38a
perspe <m.alexandre@campus.fct.unl.pt>
2022-02-28 15:27:21 +0000

    Update Import material dialog to better understand how data will be imported

--- smm_uis/db_window.py
+++ smm_uis/db_window.py
@@ -1,18 +1,235 @@
 """ Class for the DB Window """
 import logging
 import os
+from typing import Union, Any
 
 from PyQt5 import QtGui
-from PyQt5.QtGui import QStandardItem, QStandardItemModel
-from PyQt5.QtWidgets import QFileDialog, QMessageBox, QTableWidget, QWidget
+from PyQt5.QtCore import QAbstractTableModel, QRegExp, Qt
+from PyQt5.QtGui import QRegExpValidator, QStandardItem, QStandardItemModel
+from PyQt5.QtWidgets import QFileDialog, QMessageBox, QWidget
 from modules.fig_class import FigWidget, PltFigure
 import numpy as np
+import pandas as pd
+from pandas.errors import ParserError
 from scipy.interpolate import interp1d
 
 from .smm_database_window import Ui_Database
 from .smm_import_db_mat import Ui_ImportDB
 
 Units = {"nm": 1, "um": 1e3, "mm": 1e6}
+Decimal = {"Dot (.)": ".", "Comma (,)": ","}
+Separator = {"comma (,)": ",", "space ( )": " ", ";": ";", "other": " "}
+""" Model to show a preview of the imported data """
+
+
+class TableModel(QAbstractTableModel):
+    """ Abstract model to implement for the imported data preview
+    This is built to work with pandas DFs
+    """
+    def __init__(self, data) -> None:
+        super().__init__()
+        self._data = data
+
+    def data(self, index, role) -> Union[str, None]:
+        if role == Qt.DisplayRole:
+            value = self._data.iloc[index.row(), index.column()]
+            return str(value)
+
+    def rowCount(self, _) -> int:
+        return self._data.shape[0]
+
+    def columnCount(self, _) -> int:
+        return self._data.shape[1]
+
+    def headerData(self, section, orientation, role) -> Union[str, None]:
+        if role == Qt.DisplayRole:
+            if orientation == Qt.Horizontal:
+                return f"Column {section}"
+            if orientation == Qt.Vertical:
+                return str(self._data.index[section])
+
+
+class ImportWindow(QWidget):
+    def __init__(self, parent):
+        self.parent = parent
+        super(ImportWindow, self).__init__()
+        self.ui = Ui_ImportDB()
+        self.ui.setupUi(self)
+        # Startup variables
+        self.filepath = None
+        self.data = None
+        self.preview_import = None
+        self.import_args = {
+            "sep": " ",
+            "decimal": ".",
+            "skiprows": 0,
+            "header": None
+        }
+        # Things for preview table
+        self.import_table = self.ui.tableView
+        self.import_model = TableModel(pd.DataFrame())
+        self.import_table.setModel(self.import_model)
+        self.ui.unit_combobox.addItems(list(Units.keys()))
+        self.initializeUI()
+
+    def initializeUI(self):
+        """ Initialize UI elements """
+        # General buttons
+        self.ui.choose_file_button.clicked.connect(self.choose_db_mat)
+        self.ui.import_button.clicked.connect(self.import_db_mat)
+        self.ui.preview_button.clicked.connect(self.preview_db_mat)
+        # Add validators to line edits
+        self.ui.ignore_lines_edit.setValidator(
+            QtGui.QIntValidator(0, 1000, self))
+        # Dont accept numbers in the other line edit
+        other_regex = QRegExp("[^0-9]+")
+        self.ui.other_edit.setValidator(QRegExpValidator(other_regex))
+        # Buttons that should update the table view preview
+        self.ui.decimal_group.buttonClicked.connect(
+            lambda btn: self.update_decimal(btn))
+        self.ui.delimiter_group.buttonClicked.connect(
+            lambda btn: self.update_delimiter(btn))
+        self.ui.other_edit.textChanged.connect(self.update_other_label)
+        self.ui.ignore_lines_edit.textChanged.connect(self.update_skiprows)
+        self.ui.unit_combobox.currentTextChanged.connect(self.update_preview)
+
+    """ Update each button group value in self.import_args """
+
+    def update_skiprows(self):
+        logging.debug(f"Update skiprows: {self.import_args['skiprows']=}")
+        number = self.ui.ignore_lines_edit.text()
+        if number == '':
+            return
+        self.import_args["skiprows"] = int(number)
+        self.update_preview()
+
+    def update_decimal(self, btn):
+        logging.debug(f"Updated decimal:{btn.text()} '{Decimal[btn.text()]}'")
+        self.import_args["decimal"] = Decimal[btn.text()]
+        self.update_preview()
+
+    def update_other_label(self):
+        other_text = self.ui.other_edit.text()
+        if other_text == '':
+            logging.debug("other text empty ignoring update")
+            return
+        self.import_args["sep"] = other_text
+        self.update_preview()
+
+    def update_delimiter(self, btn):
+        logging.debug(f"Updated Sep: {btn.text()} '{Separator[btn.text()]}'")
+        if btn.text() != "other":
+            self.ui.other_edit.setEnabled(False)
+            self.import_args["sep"] = Separator[btn.text()]
+        else:
+            # Action to update label from other_cb status
+            logging.debug("Activating QLine Edit for other option")
+            self.ui.other_edit.setEnabled(True)
+            other_text = self.ui.other_edit.text()
+            logging.debug(f"other text.... '{other_text}'")
+            if other_text == '':
+                logging.debug(f"No text in other... Aborting")
+                return
+            self.update_other_label()
+        self.update_preview()
+
+    def update_preview(self):
+        """ Update self.data and the table view representation """
+        if self.filepath is None:
+            logging.debug("No filepath chosen.. Not Updating..")
+            return
+        try:
+            self.data: pd.DataFrame = pd.read_csv(self.filepath,
+                                                  **self.import_args)
+            self.data.iloc[:, 0] *= Units[self.ui.unit_combobox.currentText()]
+        except (ParserError, TypeError) as error:
+            logging.error(f"Parse Error when importing data:\n{error=}")
+            return
+        self.import_model._data = self.data
+        self.import_model.layoutChanged.emit()
+
+    """ Functions for the main buttons """
+
+    def choose_db_mat(self):
+        """
+        Open a AskFileDialog to choose a file with the material data and
+        update the qtextlabel with the name of the file
+        """
+        filepath = QFileDialog.getOpenFileName(self, 'Open File')
+        logging.debug(f"Chosen filepath: {filepath}")
+        if filepath[0] == '':
+            return
+        self.filepath = filepath[0]
+        filename = os.path.basename(filepath[0])
+        self.update_preview()
+        self.ui.chosen_file_label.setText(filename)
+
+    def preview_db_mat(self):
+        """
+        Show a preview of the imported data with the interpolation
+        """
+        if self.data is None:
+            QMessageBox.information(self, "Choose File",
+                                    "Must select a file before previewing",
+                                    QMessageBox.Ok, QMessageBox.Ok)
+            return
+        data = self.data.values
+        try:
+            lmb, n, k = data[:, 0], data[:, 1], data[:, 2]
+        except IndexError as error:
+            logging.error(f"{error=}")
+            return
+        interp_n = interp1d(lmb, n)
+        interp_k = interp1d(lmb, k)
+        # Plot the results
+        # Preview chosen material
+        self.preview_import = FigWidget("Preview New Material")
+        self.preview_import_fig = PltFigure(self.preview_import.layout,
+                                            "Wavelength (nm)",
+                                            "n/k",
+                                            width=7)
+        self.n_plot = self.preview_import_fig.axes
+        self.n_plot.set_ylabel("n")
+        self.n_plot.plot(lmb, n, "b.", label="n")
+        self.n_plot.plot(lmb, interp_n(lmb), "b", label="interp n")
+        self.k_plot = self.n_plot.twinx()
+        self.k_plot.set_ylabel("k")
+        self.k_plot.plot(lmb, k, "r.", label="k")
+        self.k_plot.plot(lmb, interp_k(lmb), "r", label="interp k")
+        self.n_plot.legend(loc="upper right")
+        self.k_plot.legend(loc="center right")
+        self.preview_import.show()
+
+    def import_db_mat(self):
+        """
+        Add the chosen material to the database
+        """
+        mat_name = self.ui.mat_name_edit.text()
+        if self.data is None:
+            QMessageBox.information(self, "Choose File",
+                                    "Must select a file before importing",
+                                    QMessageBox.Ok, QMessageBox.Ok)
+            return
+        elif mat_name == '':
+            QMessageBox.information(
+                self, "No material name",
+                "Please select a material name before importing",
+                QMessageBox.Ok, QMessageBox.Ok)
+            return
+        data = self.data.values
+        self.parent.database.add_content(mat_name, data)
+        QMessageBox.information(self, "Import Successful",
+                                "Material imported successfully",
+                                QMessageBox.Ok, QMessageBox.Ok)
+        # Update the database previews
+        self.close()
+
+    def closeEvent(self, a0: QtGui.QCloseEvent) -> None:
+        if self.preview_import is not None:
+            self.preview_import.close()
+        self.parent.update_db_preview()
+        self.parent.update_mat_comboboxes()
+        return super().closeEvent(a0)
 
 
 class DBWindow(QWidget):
@@ -72,16 +289,8 @@ class DBWindow(QWidget):
         """
         Open a new UI to manage importing new data
         """
-        self.new_mat = np.array([])
-        self.db_import_window = QTableWidget()
-        self.db_import_ui = Ui_ImportDB()
-        self.db_import_ui.setupUi(self.db_import_window)
+        self.db_import_window = ImportWindow(self)
         self.db_import_window.show()
-        self.db_import_ui.unit_combobox.addItems(list(Units.keys()))
-        self.db_import_ui.choose_file_button.clicked.connect(
-            self.choose_db_mat)
-        self.db_import_ui.import_button.clicked.connect(self.import_db_mat)
-        self.db_import_ui.preview_button.clicked.connect(self.preview_db_mat)
 
     def rmv_db_material(self):
         """
@@ -155,105 +364,6 @@ class DBWindow(QWidget):
         self.n_plot.legend(loc="upper right")
         self.k_plot.legend(loc="center right")
         self.preview_choice.show()
-
-    """ Functions for the Add button """
-
-    def choose_db_mat(self):
-        """
-        Open a AskFileDialog to choose a file with the material data and
-        update the qtextlabel with the name of the file
-        """
-        filepath = QFileDialog.getOpenFileName(self, 'Open File')
-        if filepath[0] == '':
-            return
-        if self.db_import_window is None:
-            raise Exception("Unknown Error...")
-        logging.debug("Chosen Unit: {chosen_unit}")
-        filename = os.path.basename(filepath[0])
-        logging.debug("Getting data for new material")
-        self.new_mat = self.parent.get_data_from_file(filepath[0])
-        if self.new_mat.shape[0] == 0:
-            logging.debug("No data in file... Ignoring")
-            return
-        if self.new_mat.shape[1] < 3:
-            title = "Incomplete import"
-            msg = "The data must have 3 columns (wavelength/n/k)"
-            QMessageBox.warning(self, title, msg, QMessageBox.Close,
-                                QMessageBox.Close)
-            self.new_mat = np.array([])
-        else:
-            self.db_import_ui.chosen_file_label.setText(filename)
-        self.db_import_window.raise_()
-        self.db_import_window.setFocus(True)
-        self.db_import_window.activateWindow()
-
-    def preview_db_mat(self):
-        """
-        Show a preview of the imported data with the interpolation
-        """
-        if self.db_import_window is None:
-            raise Exception("Unknown Error...")
-        if self.new_mat.shape[0] == 0:
-            QMessageBox.information(self, "Choose File",
-                                    "Must select a file before previewing",
-                                    QMessageBox.Ok, QMessageBox.Ok)
-            return
-        lmb, n, k = self.new_mat[:, 0], self.new_mat[:, 1], self.new_mat[:, 2]
-        interp_n = interp1d(lmb, n)
-        interp_k = interp1d(lmb, k)
-        # Plot the results
-        # Preview chosen material
-        self.preview_import = FigWidget("Preview New Material")
-        self.preview_import_fig = PltFigure(self.preview_import.layout,
-                                            "Wavelength (File Units)",
-                                            "n/k",
-                                            width=7)
-        self.n_plot = self.preview_import_fig.axes
-        self.n_plot.set_ylabel("n")
-        self.n_plot.plot(lmb, n, "b.", label="n")
-        self.n_plot.plot(lmb, interp_n(lmb), "b", label="interp n")
-        self.k_plot = self.n_plot.twinx()
-        self.k_plot.set_ylabel("k")
-        self.k_plot.plot(lmb, k, "r.", label="k")
-        self.k_plot.plot(lmb, interp_k(lmb), "r", label="interp k")
-        self.n_plot.legend(loc="upper right")
-        self.k_plot.legend(loc="center right")
-        self.preview_import.show()
-
-    def import_db_mat(self):
-        """
-        Add the chosen material to the database
-        """
-        if self.db_import_window is None:
-            raise Exception("Unknown Error...")
-        chosen_unit = self.db_import_ui.unit_combobox.currentText()
-        mat_name = self.db_import_ui.mat_name_edit.text()
-        if self.new_mat.shape[0] == 0:
-            QMessageBox.information(self, "Choose File",
-                                    "Must select a file before importing",
-                                    QMessageBox.Ok, QMessageBox.Ok)
-            return
-        elif mat_name == '':
-            QMessageBox.information(
-                self, "No material name",
-                "Please select a material name before importing",
-                QMessageBox.Ok, QMessageBox.Ok)
-            return
-        self.new_mat[:, 0] *= Units[chosen_unit]
-        print(self.new_mat[:, 0])
-        self.database.add_content(mat_name, self.new_mat)
-        self.new_mat = np.array([])
-        self.db_import_window.close()
-        QMessageBox.information(self, "Import Successful",
-                                "Material imported successfully",
-                                QMessageBox.Ok, QMessageBox.Ok)
-        # Update the database previews
-        self.update_db_preview()
-        # Update the comboboxes with the materials
-        self.update_mat_comboboxes()
-        self.raise_()
-        self.setFocus(True)
-        self.activateWindow()
 
     def closeEvent(self, a0: QtGui.QCloseEvent) -> None:
         if self.db_import_window is not None:
