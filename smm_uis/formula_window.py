07a16002b1d1d15201236b3809fe65340b641c63
perspe <m.alexandre@campus.fct.unl.pt>
2022-03-12 22:44:05 +0000

    Update general setup of files Add more dispersion formulas (Cauchy Abs/Sellmeier Abs) Bugs

--- smm_uis/formula_window.py
+++ smm_uis/formula_window.py
@@ -1,9 +1,16 @@
 import logging
+from typing import List, Union
 
-from PyQt5 import Qt, QtCore
+from PyQt5 import QtCore
 from PyQt5.QtGui import QRegExpValidator
-from PyQt5.QtWidgets import QMainWindow, QMessageBox, QSizePolicy, QSpacerItem, QWidget
+from PyQt5.QtWidgets import (
+    QMainWindow,
+    QMessageBox,
+    QSizePolicy,
+    QSpacerItem,
+)
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT
+from modules.dispersion import cauchy, cauchy_abs, const, sellmeier_abs, tauc_lorentz_n
 from modules.fig_class import PltFigure
 import numpy as np
 import scipy.constants as scc
@@ -11,108 +18,16 @@ import scipy.constants as scc
 from .custom_widgets import CustomSlider
 from .smm_formula_mat import Ui_Formula
 
-
-def const(e, n, k):
-    """ Formula for a constant refractive index """
-    arr = np.ones_like(e, np.float64)
-    return arr * n, arr * k
-
-
-def tauc_lorentz_peak(e, eg, e0, a, c):
-    """ Formula to calcuate one peak for the Tauc Lorentz formula """
-    logging.debug(f"{eg=}::{e0=}::{a=}::{c=}")
-    ei = np.zeros_like(e, dtype=np.float64)
-    er = np.zeros_like(e, dtype=np.float64)
-    # Interim variables
-    e2, eg2, e02, c2 = np.power(e, 2), eg**2, e0**2, c**2
-    gamma = np.sqrt(e02 - c2 / 2)
-    alpha = np.sqrt(4 * e02 - c2)
-    zeta4 = np.power(e2 - gamma**2, 2) + alpha**2 * c2 / 4
-    ain = (eg2 - e02) * e2 + eg2 * c2 - e02 * (e02 + 3 * eg2)
-    atan = (e2 - e02) * (e02 + eg2) + eg2 * c2
-    # Proceed to calculation
-    eg_mask = e > eg
-    # Imaginary part
-    ei[eg_mask] = (1 / e[eg_mask]) * (a * e0 * c * (e[eg_mask] - eg)**2) / (
-        (e2[eg_mask] - e02)**2 + c2 * e2[eg_mask])
-    # Real part
-    er += (a * c * ain / (2 * np.pi * zeta4 * alpha * e0)) * np.log(
-        (e02 + eg2 + alpha * eg) / (e02 + eg2 - alpha * eg))
-    er -= (a * atan) / (np.pi * zeta4 * e0) * (np.pi - np.arctan(
-        (2 * eg + alpha) / c) + np.arctan((alpha - 2 * eg) / c))
-    er += (4 * a * e0 * eg * (e2 - gamma**2) /
-           (np.pi * zeta4 * alpha)) * (np.arctan(
-               (alpha + 2 * eg) / c) + np.arctan((alpha - 2 * eg) / c))
-    er -= (a * e0 * c * (e2 + eg2) /
-           (np.pi * zeta4 * e)) * np.log(np.abs(e - eg) / (e + eg))
-    er += (2 * a * e0 * c * eg / (np.pi * zeta4)) * np.log(
-        np.abs(e - eg) * (e + eg) / (np.sqrt((e02 - eg2)**2 + eg2 * c2)))
-    return er, ei
-
-
-def tauc_lorentz_1(e, einf, eg, e0, a, c):
-    """ Tauc Lorentz Equation with one peak """
-    er, ei = tauc_lorentz_peak(e, eg, e0, a, c)
-    er += einf
-    e_complex = er + 1j * ei
-    n = np.sqrt(e_complex)
-    return np.real(n), np.imag(n)
-
-
-def tauc_lorentz_2(e, einf, eg, e01, a1, c1, e02, a2, c2):
-    """ Tauc Lorentz Equation with one peak """
-    er1, ei1 = tauc_lorentz_peak(e, eg, e01, a1, c1)
-    er2, ei2 = tauc_lorentz_peak(e, eg, e02, a2, c2)
-    er = er1 + er2 + einf
-    ei = ei1 + ei2
-    e_complex = er + 1j * ei
-    n = np.sqrt(e_complex)
-    return np.real(n), np.imag(n)
-
-
-def tauc_lorentz_n(e, n_peak, einf, eg, *args):
-    """ Tauc Lorentx Equation for multiple peaks
-    Args:
-        e (array): energy array
-        n_peak (int): Number of peaks
-        einf (float): einf variable
-        eg (float): eg variable
-        *args (e0, a, c)*n_peak: remaining peak variables
-    """
-    n_peak = int(n_peak)
-    logging.debug(f"{n_peak=}::{einf=}::{eg=}::{args=}")
-    if len(args) != 3 * n_peak:
-        logging.error(args, n_peak)
-        raise Exception("Number of arguments not compatible with n_peak")
-    er = np.zeros_like(e, dtype=np.float64)
-    ei = np.zeros_like(e, dtype=np.float64)
-    for i in range(n_peak):
-        e0i, ai, ci = args[i * 3 + 0], args[i * 3 + 1], args[i * 3 + 2]
-        er_i, ei_i = tauc_lorentz_peak(e, eg, e0i, ai, ci)
-        er += er_i
-        ei += ei_i
-    er += einf
-    e_complex = er + 1j * ei
-    n = np.sqrt(e_complex)
-    return np.real(n), np.imag(n)
-
-
-def cauchy(e, a, b, c):
-    """ Standard non-absorber cauchy formula """
-    logging.debug(f"{a=}::{b=}::{c=}")
-    lmb = (scc.h * scc.c) / (scc.e * e) * 1e9
-    logging.debug(lmb)
-    n = a + 1e4 * b / lmb**2 + 1e9 * c / lmb**4
-    k = np.zeros_like(n)
-    return n, k
-
-
+# Dictionary with all the dispersion equations
 methods: dict = {
     "Constant": const,
     "Tauc Lorentz": tauc_lorentz_n,
-    "Cauchy": cauchy
+    "Cauchy": cauchy,
+    "Cauchy Absorbent": cauchy_abs,
+    "Sellmeier Absorbent": sellmeier_abs
 }
 
+# Alias to convert nm to ev and ev to nm
 _nm_to_ev = (scc.h * scc.c) / (scc.e * 1e-9)
 Units = {
     "Energy (eV)": ("Emin (eV)", "Emax (eV)"),
@@ -121,8 +36,8 @@ Units = {
 
 
 class FormulaWindow(QMainWindow):
-    def __init__(self, parent=None):
-        self.parent = parent
+    def __init__(self, parent: Union[QtCore.QObject, None]=None) -> None:
+        self.parent: Union[QtCore.QObject, None] = parent
         super(FormulaWindow, self).__init__()
         logging.debug("Opened Formula Window")
         self.ui = Ui_Formula()
@@ -130,26 +45,25 @@ class FormulaWindow(QMainWindow):
         # Internal variables of interest
         # Connect Combobox names to functions
         self.methods: dict = {
-            "Constant": self.method_const,
-            "Tauc Lorentz": self.method_TL,
-            "Cauchy": self.cauchy
+            "Constant": self.MConst,
+            "Tauc Lorentz": self.MTaucLorentz,
+            "Cauchy": self.MCauchy,
+            "Cauchy Absorbent": self.MCauchyAbs,
+            "Sellmeier Absorbent": self.MSellmeierAbs
         }
-        self.slider_list = []
+        self.slider_list: List[CustomSlider] = []
         # Fill comboboxes
         self.ui.units_cb.addItems([str(unit) for unit in Units.keys()])
         self.ui.method_cb.addItems([key for key in self.methods.keys()])
         # Define the xdata, xlims and the xvariable
-        self._xmin = 0.5
-        self._xmax = 6.5
-        self._xres = 500
+        self._xmin: float = 0.5
+        self._xmax: float = 6.5
+        self._xres: int = 500
         self._e = np.linspace(self._xmin,
                               self._xmax,
                               self._xres,
                               dtype=np.float64)
         self._lmb = _nm_to_ev * (1 / self._e)
-        self.xlim = [self.ui.xmin_value, self.ui.xmax_value]
-        self.xlim[0].setText(str(self._xmin))
-        self.xlim[1].setText(str(self._xmax))
         # Create the figure to plot the data (with 2 y axis for n and k)
         self.plot_canvas = PltFigure(self.ui.plot_layout,
                                      self.ui.units_cb.currentText(), "n")
@@ -177,7 +91,7 @@ class FormulaWindow(QMainWindow):
         self._n_plot = nplot[0]
         self._k_plot = kplot[0]
 
-    def initializeUI(self):
+    def initializeUI(self) -> None:
         """ Connect functions to buttons """
         # Add validators to QLineEdits
         double_regex = QtCore.QRegExp("[0-9]+\\.?[0-9]*j?")
@@ -192,7 +106,7 @@ class FormulaWindow(QMainWindow):
 
     """ Update the n/k info and the plot """
 
-    def update_method(self):
+    def update_method(self) -> None:
         """ Update the formula """
         new_method: str = self.ui.method_cb.currentText()
         logging.debug(f"Method Updated to: {new_method}")
@@ -200,21 +114,21 @@ class FormulaWindow(QMainWindow):
         self._update_nk()
         self._rebuild_plot()
 
-    def _update_nk(self):
+    def _update_nk(self) -> None:
         """ Get updated nk values for current given parameters """
         curr_method: str = self.ui.method_cb.currentText()
         curr_values = tuple(
             [slider_i.curr_value() for slider_i in self.slider_list])
         self._n, self._k = methods[curr_method](self._e, *curr_values)
 
-    def _update_plot(self):
+    def _update_plot(self) -> None:
         """ Calculate n/k from variables in widget and plot """
         self._update_nk()
         self._n_plot.set_ydata(self._n)
         self._k_plot.set_ydata(self._k)
         self.plot_canvas.draw()
 
-    def _rebuild_plot(self):
+    def _rebuild_plot(self) -> None:
         """ Rebuild plot after xvar or xlims is changed """
         logging.debug(self.ui.units_cb.currentText())
         self.n_plot.set_xlabel(self.ui.units_cb.currentText())
@@ -233,7 +147,10 @@ class FormulaWindow(QMainWindow):
 
     """ Window buttons functions """
 
-    def add_database(self):
+    def add_database(self) -> None:
+        if self.parent is None:
+            logging.critical("No parent to add data to database """)
+            raise Exception("Critical Error")
         mat_name = self.ui.mat_name.text()
         logging.debug(f"{mat_name=}")
         if mat_name == '':
@@ -264,9 +181,9 @@ class FormulaWindow(QMainWindow):
         self.parent.update_db_preview()
         self.parent.update_mat_comboboxes()
 
-    def update_xvar(self):
+    def update_xvar(self) -> None:
         """ Update the overall representation of xvar, between E and Wvl """
-        new_var = self.ui.units_cb.currentText()
+        new_var: str = self.ui.units_cb.currentText()
         min_label, max_label = Units[new_var]
         logging.debug(f"{new_var=}::{min_label=}::{max_label=}")
         # Update label text
@@ -283,11 +200,11 @@ class FormulaWindow(QMainWindow):
         # Rebuild plot
         self._rebuild_plot()
 
-    def update_xlim(self):
+    def update_xlim(self) -> None:
         """ Update the xlims for calculation """
-        curr_var = self.ui.units_cb.currentText()
-        xmin = float(self.ui.xmin_value.text())
-        xmax = float(self.ui.xmax_value.text())
+        curr_var: str = self.ui.units_cb.currentText()
+        xmin: float = float(self.ui.xmin_value.text())
+        xmax: float = float(self.ui.xmax_value.text())
         if curr_var == list(Units.keys())[0]:
             # For energy
             logging.debug(f"Update Energy: {curr_var=}::{xmin=}::{xmax=}")
@@ -304,14 +221,14 @@ class FormulaWindow(QMainWindow):
 
     """ Functions to Build the Variables for Different Methods """
 
-    def _clear_variable_layout(self):
+    def _clear_variable_layout(self) -> None:
         """ Function to clear all the widgets in the layout """
         while self.ui.variable_layout.count():
             child = self.ui.variable_layout.takeAt(0)
             if child.widget():
                 child.widget().deleteLater()
 
-    def _update_layout(self, layout):
+    def _update_layout(self, layout) -> None:
         """ Update the slider layout """
         for slider in self.slider_list:
             layout.addWidget(slider)
@@ -320,7 +237,7 @@ class FormulaWindow(QMainWindow):
                                      QSizePolicy.Expanding)
         layout.addItem(verticalSpacer)
 
-    def method_const(self):
+    def MConst(self):
         """ Create the variables for the const method
         Vars: n, k
         """
@@ -333,10 +250,10 @@ class FormulaWindow(QMainWindow):
         self.slider_list = [n, k]
         self._update_layout(layout)
 
-    def _update_tl_peaks(self):
+    def _update_tl_peaks(self) -> None:
         """ Update the number of custom Sliders for the peaks """
         self._clear_variable_layout()
-        n_peaks = int(self.slider_list[0].curr_value())
+        n_peaks: int = int(self.slider_list[0].curr_value())
         logging.debug(f"Updating TL peaks... {n_peaks=}")
         # Rebuild all widgets
         n_peak = CustomSlider("N peak", n_peaks, 1, 11, fixed_lim=True)
@@ -356,7 +273,7 @@ class FormulaWindow(QMainWindow):
         logging.debug(f"Slider List... {self.slider_list}")
         self._update_plot()
 
-    def method_TL(self):
+    def MTaucLorentz(self) -> None:
         """ Create the variables for the const method
         Vars: einf, eg, e0, a, c
         """
@@ -375,10 +292,7 @@ class FormulaWindow(QMainWindow):
         self.slider_list.insert(0, n_peak)
         logging.debug(f"Slider List... {self.slider_list}")
 
-    def cauchy(self):
-        """ Create the variables for the const method
-        Vars: A, B, C
-        """
+    def MCauchy(self) -> None:
         self._clear_variable_layout()
         layout = self.ui.variable_layout
         a = CustomSlider("A", 1, 1, 5)
@@ -386,3 +300,27 @@ class FormulaWindow(QMainWindow):
         c = CustomSlider("C", 3, 0.5, 10)
         self.slider_list = [a, b, c]
         self._update_layout(layout)
+
+    def MCauchyAbs(self) -> None:
+        self._clear_variable_layout()
+        layout = self.ui.variable_layout
+        a = CustomSlider("A", 1, 1, 5)
+        b = CustomSlider("B", 2, 0.5, 10)
+        c = CustomSlider("C", 3, 0.5, 10)
+        d = CustomSlider("D", 3, 0.5, 10)
+        e = CustomSlider("E", 3, 0.5, 10)
+        f = CustomSlider("F", 3, 0.5, 10)
+        self.slider_list = [a, b, c, d, e, f]
+        self._update_layout(layout)
+
+    def MSellmeierAbs(self) -> None:
+        self._clear_variable_layout()
+        layout = self.ui.variable_layout
+        a = CustomSlider("A", 1, 1, 5)
+        b = CustomSlider("B", 2, 0.5, 10)
+        c = CustomSlider("C", 3, 0.5, 10)
+        d = CustomSlider("D", 3, 0.5, 10)
+        e = CustomSlider("E", 3, 0.5, 10)
+        self.slider_list = [a, b, c, d, e]
+        self._update_layout(layout)
+
