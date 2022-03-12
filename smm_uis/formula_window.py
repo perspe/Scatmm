b5f0398b78a0bed408562fa8bf928a7c87a33413
perspe <m.alexandre@campus.fct.unl.pt>
2022-03-12 18:26:53 +0000

    Formula window change from Energy to Wavelength and adjust limits for energy and wavelength

--- smm_uis/formula_window.py
+++ smm_uis/formula_window.py
@@ -1,7 +1,7 @@
 import logging
-import sys
 
 from PyQt5 import Qt, QtCore
+from PyQt5.QtGui import QRegExpValidator
 from PyQt5.QtWidgets import QMainWindow, QSizePolicy, QSpacerItem, QWidget
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT
 from modules.fig_class import PltFigure
@@ -69,6 +69,29 @@ def tauc_lorentz_2(e, einf, eg, e01, a1, c1, e02, a2, c2):
     n = np.sqrt(e_complex)
     return np.real(n), np.imag(n)
 
+def tauc_lorentz_n(e, n_peak, einf, eg, *args):
+    """ Tauc Lorentx Equation for multiple peaks
+    Args:
+        e (array): energy array
+        n_peak (int): Number of peaks
+        einf (float): einf variable
+        eg (float): eg variable
+        *args (e0, a, c)*n_peak: remaining peak variables
+    """
+    if len(args) < 3*n_peak:
+        raise Exception("Number of arguments not compatible with n_peak")
+    er = np.zeros_like(e, dtype=np.float64)
+    ei = np.zeros_like(e, dtype=np.float64)
+    for i in range(n_peak):
+        e0i, ai, ci = args[i*3+0], args[i*3+1], args[i*3+2]
+        er_i, ei_i = tauc_lorentz_peak(e, eg, e0i, ai, ci)
+        er += er_i
+        ei += ei_i
+    er += einf
+    e_complex = er + 1j * ei
+    n = np.sqrt(e_complex)
+    return np.real(n), np.imag(n)
+
 
 def cauchy(e, a, b, c):
     """ Standard non-absorber cauchy formula """
@@ -87,6 +110,13 @@ methods: dict = {
     "Cauchy": cauchy
 }
 
+_nm_to_ev = (scc.h * scc.c) / (scc.e * 1e-9)
+# _ev_to_nm = (scc.h * scc.c) / (scc.e * 1e-9)
+Units = {
+    "Energy (eV)": ("Emin (eV)", "Emax (eV)"),
+    "Wavelength (nm)": ("λmin (nm)", "λmax (nm)")
+}
+
 
 class FormulaWindow(QMainWindow):
     def __init__(self, parent=None):
@@ -96,77 +126,145 @@ class FormulaWindow(QMainWindow):
         self.ui = Ui_Formula()
         self.ui.setupUi(self)
         # Internal variables of interest
+        # Connect Combobox names to functions
         self.methods: dict = {
             "Constant": self.method_const,
             "Tauc Lorentz 1": self.method_TL_1,
             "Tauc Lorentz 2": self.method_TL_2,
             "Cauchy": self.cauchy
         }
-        self._e = np.linspace(0.5, 6.5, 500, dtype=np.float64)
-        self._lmb = (scc.c * scc.h) / (scc.e *
-                                       self._e) * 1e9  # Wavelength in nm
         self.slider_list = []
-        # Create the figure to plot the data
+        # Fill comboboxes
+        self.ui.units_cb.addItems([str(unit) for unit in Units.keys()])
+        self.ui.method_cb.addItems([key for key in self.methods.keys()])
+        # Define the xdata, xlims and the xvariable
+        self._xmin = 0.5
+        self._xmax = 6.5
+        self._xres = 500
+        self._e = np.linspace(self._xmin,
+                              self._xmax,
+                              self._xres,
+                              dtype=np.float64)
+        self._lmb = _nm_to_ev * (1 / self._e)
+        self.xlim = [self.ui.xmin_value, self.ui.xmax_value]
+        self.xlim[0].setText(str(self._xmin))
+        self.xlim[1].setText(str(self._xmax))
+        # Create the figure to plot the data (with 2 y axis for n and k)
         self.plot_canvas = PltFigure(self.ui.plot_layout,
-                                     "Energy (eV)",
-                                     "n",
-                                     width=7)
+                                     self.ui.units_cb.currentText(), "n")
+        self.addToolBar(QtCore.Qt.TopToolBarArea,
+                        NavigationToolbar2QT(self.plot_canvas, self))
         self.n_plot = self.plot_canvas.axes
         self.n_plot.set_ylabel("n", color='b')
         self.n_plot.set_ylim((0, 5))
         self.k_plot = self.n_plot.twinx()
         self.k_plot.set_ylabel("k", color="r")
         self.k_plot.set_ylim((0, 5))
-        # Update Combobox with name
-        self.ui.method_cb.addItems([key for key in self.methods.keys()])
+        # InitializeUI and update plot info
         self.initializeUI()
-        self.update_method(True)
-        # Make the initialization plot
-        self._n, self._k = self.update_nk()
+        # Make the first plot iteration
+        self.methods[self.ui.method_cb.currentText()]()
+        self._update_nk()
         nplot = self.n_plot.plot(self._e, self._n, c='b')
         kplot = self.k_plot.plot(self._e, self._k, c='r')
         self._n_plot = nplot[0]
         self._k_plot = kplot[0]
-        self.addToolBar(QtCore.Qt.TopToolBarArea,
-                        NavigationToolbar2QT(self.plot_canvas, self))
 
     def initializeUI(self):
         """ Connect functions to buttons """
-        self.ui.method_cb.currentIndexChanged.connect(
-            lambda a: self.update_method(False))
+        # Add validators to QLineEdits
+        double_regex = QtCore.QRegExp("[0-9]+\\.?[0-9]*j?")
+        self.ui.xmin_value.setValidator(QRegExpValidator(double_regex))
+        self.ui.xmax_value.setValidator(QRegExpValidator(double_regex))
+        # Connect buttons and signals to functions
+        self.ui.method_cb.currentIndexChanged.connect(self.update_method)
         self.ui.add_db_button.clicked.connect(self.add_database)
+        self.ui.units_cb.currentIndexChanged.connect(self.update_xvar)
+        self.ui.xmin_value.editingFinished.connect(self.update_xlim)
+        self.ui.xmax_value.editingFinished.connect(self.update_xlim)
 
     """ Update the n/k info and the plot """
 
-    def update_method(self, first_run=False):
+    def update_method(self):
         """ Update the formula """
         new_method: str = self.ui.method_cb.currentText()
         logging.debug(f"Method Updated to: {new_method}")
-        # Update the variables in methods
         self.methods[new_method]()
-        if first_run is False:
-            logging.debug("Updating plot for new method")
-            self.update_plot()
+        self._rebuild_plot()
 
-    def update_nk(self):
+    def _update_nk(self):
         """ Get updated nk values for current given parameters """
         curr_method: str = self.ui.method_cb.currentText()
         curr_values = tuple(
             [slider_i.curr_value() for slider_i in self.slider_list])
-        n, k = methods[curr_method](self._e, *curr_values)
-        return n, k
+        self._n, self._k = methods[curr_method](self._e, *curr_values)
 
-    def update_plot(self):
+    def _update_plot(self):
         """ Calculate n/k from variables in widget and plot """
-        self._n, self._k = self.update_nk()
+        self._update_nk()
         self._n_plot.set_ydata(self._n)
         self._k_plot.set_ydata(self._k)
+        self.plot_canvas.draw()
+
+    def _rebuild_plot(self):
+        """ Rebuild plot after xvar or xlims is changed """
+        logging.debug(self.ui.units_cb.currentText())
+        self.n_plot.set_xlabel(self.ui.units_cb.currentText())
+        if self.ui.units_cb.currentText() == list(Units.keys())[0]:
+            self._n_plot.set_xdata(self._e)
+            self._n_plot.set_ydata(self._n)
+            self._k_plot.set_xdata(self._e)
+            self._k_plot.set_ydata(self._k)
+        else:
+            self._n_plot.set_xdata(self._lmb)
+            self._n_plot.set_ydata(self._n)
+            self._k_plot.set_xdata(self._lmb)
+            self._k_plot.set_ydata(self._k)
+        self.n_plot.set_xlim(self._xmin, self._xmax)
         self.plot_canvas.draw()
 
     """ Window buttons functions """
 
     def add_database(self):
+        # TODO:  <12-03-22, yourname> #
         return
+
+    def update_xvar(self):
+        """ Update the overall representation of xvar, between E and Wvl """
+        new_var = self.ui.units_cb.currentText()
+        min_label, max_label = Units[new_var]
+        logging.debug(f"{new_var=}::{min_label=}::{max_label=}")
+        # Update label text
+        self.ui.xmin_label.setText(min_label)
+        self.ui.xmax_label.setText(max_label)
+        limit_1, limit_2 = _nm_to_ev * (1 / self._xmin), _nm_to_ev * (1 / self._xmax)
+        # Update QLineEdit Limits
+        self._xmin = min([limit_1, limit_2])
+        self._xmax = max([limit_1, limit_2])
+        logging.debug(f"{self._xmin=}::{self._xmax=}")
+        self.ui.xmin_value.setText(f"{self._xmin:.1f}")
+        self.ui.xmax_value.setText(f"{self._xmax:.1f}")
+        # Rebuild plot
+        self._rebuild_plot()
+
+    def update_xlim(self):
+        """ Update the xlims for calculation """
+        curr_var = self.ui.units_cb.currentText()
+        xmin = float(self.ui.xmin_value.text())
+        xmax = float(self.ui.xmax_value.text())
+        if curr_var == list(Units.keys())[0]:
+            # For energy
+            logging.debug(f"Update Energy: {curr_var=}::{xmin=}::{xmax=}")
+            self._e = np.linspace(xmin, xmax, self._xres)
+            self._lmb = _nm_to_ev * (1 / self._e)
+            self._xmin, self._xmax = np.min(self._e), np.max(self._e)
+        else:
+            logging.debug(f"Update Wavelength: {curr_var=}::{xmin=}::{xmax=}")
+            self._lmb = np.linspace(xmin, xmax, self._xres)
+            self._e = _nm_to_ev * (1 / self._lmb)
+            self._xmin, self._xmax = np.min(self._lmb), np.max(self._lmb)
+        self._update_nk()
+        self._rebuild_plot()
 
     """ Functions to Build the Variables for Different Methods """
 
@@ -181,7 +279,7 @@ class FormulaWindow(QMainWindow):
         """ Update the slider layout """
         for slider in self.slider_list:
             layout.addWidget(slider)
-            slider.changed.connect(self.update_plot)
+            slider.changed.connect(self._update_plot)
         verticalSpacer = QSpacerItem(20, 80, QSizePolicy.Minimum,
                                      QSizePolicy.Expanding)
         layout.addItem(verticalSpacer)
@@ -192,8 +290,8 @@ class FormulaWindow(QMainWindow):
         """
         self._clear_variable_layout()
         layout = self.ui.variable_layout
-        n = CustomSlider("n", 1, 10)
-        k = CustomSlider("k")
+        n = CustomSlider("n", 1, 5)
+        k = CustomSlider("k", 0, 5)
         layout.addWidget(n)
         layout.addWidget(k)
         self.slider_list = [n, k]
